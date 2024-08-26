956e9c6b142ddf8bcacb7428ee1e1c26639f6fd1
RaghuRumble <yash.19.r04@gmail.com>
2024-08-26 13:28:01 +0530

    Stacks Stacks

---
+++ Stacks and Queues/templateUse.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+#include "pair.cpp"
+
+int main(){
+    pair<int> p1;
+    p1.setX(10);
+    p1.setY(20);
+
+    cout<<p1.getX()<<" "<<p1.getY()<<endl;
+
+    pair<double> p2;
+    p2.setX(100.34);
+    p2.setY(34.21);
+    cout<< p2.getX()<<" "<<p2.getY()<<endl;
+
+    pair<char> p3;
+}
